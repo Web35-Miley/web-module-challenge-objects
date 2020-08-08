07b394211f175bd1715fe7d800e311d7ca7a24d8
Miley Wright <dishaliekwright@yahoo.com>
2020-08-08 00:55:36 -0400

    MVP

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 ///////////////Menu Items (MVP)///////////////////
 
 const latte = {name: "Cafe Latte", price: 4, category: "Drinks"};
-const burger = {name: "Burger", price: 18, category: "Lunch"};
+const Burger = {name: "Burger", price: 18, category: "Lunch"};
 const breakfastBurrito = {name: "Breakfast Burrito", price: 16, category:"Breakfast"};
 
 /* Task 1a: write a function to return more menu items with the same format as the items above. */
 
 function createMenuItem(name, cost, category){
-    /* Code here */
+    return {name, cost, category}
 }
 
 /* Task 1b: use your function to create 3 more menu items. You may add any items to the menu that you'd like */
 
-
+console.log(createMenuItem("Boba Tea", 4.50, "Drinks"))
+console.log(createMenuItem("Chocolate Covered Strawberries", 12, "Dessert"))
+console.log(createMenuItem("Salmon", 14.50, "Dinner"))
 
 /* Task 2: You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to your burger object that automatically calculates price given a string as a parameter. 
 
@@ -23,8 +25,21 @@ Your method should accept:
 and should return a number. 
 
 For example, burger.discount("teacher") would return 13.5 and burger.discount("public") would return 16.2*/
+const burger = {
+  name: "Burger", 
+  price: 18, 
+  category: "Lunch", 
+  discount: function(person){
+    if (person == "teacher" || person == "student"){
+      this.price *= .75;
+    } else {
+      this.price *= .90;
+    }
+    return this.price
+  },
+};
 
-
+console.log("public",burger.discount("public"))
 
 ///////////////Reviews (MVP)///////////////////
 
@@ -39,12 +54,18 @@ const reviews = [{name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and
 ]
 
 /* Task 3: Console.log just Julius' feedback */
-
+console.log("Task 3:", reviews[5].feedback)
 
 /* Task 4: Add a new rating with your (fictitious) opinions of the restaurant in the same format as the reviews above. */
+function addReview( arr, name, rating, feedback) {
+  arr.push({name, rating, feedback})
+  return arr
+}
+console.log(addReview(reviews, "Miley", 10, "That was poppin!"))
 
-
-/* Task 5: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
+/* Task 5: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays" */
+reviews[7].feedback = "this place is chill with really cool people, great for getting work done on weekdays"
+console.log(reviews)
 
 /*  Task 6: Write a function to return a review based on the index of the review in the array.
 
@@ -59,8 +80,14 @@ and should return a string in the format `{name} gave the restaurant a {rating},
  * it will return `Daniela gave the restaurant a 5 star review and their feedback was: Beautiful atmosphere and wonderful vegan options!`
 */
 function getReviewByIndex(reviews, index) {
-    /* code here */
+    for(i = 0; i < reviews.length; i++) {
+      if ( i === index) {
+        return `${reviews[i].name} gave the restaurant a ${reviews[i].rating} star review and their feedback was: ${reviews[i].feedback}.`
+      }
+    }
   }
+
+  console.log(getReviewByIndex(reviews, 0))
   
 
 /* Task 7: Write a function to get information about the most recent review called `getLastReview`
@@ -72,10 +99,10 @@ and should return a string in the format `name} gave the restaurant a {rating}, 
 
 For example, if getLastReview is invoked passing the reviews array it will return `Reyna gave the restaurant a 3.5 star review and their feedback was: "this place is chill with really cool people, great for getting work done on weekdays"`.
 */
-function getLastReview(/* code here */) {
-    /* code here */
+function getLastReview(reviews) {
+    return reviews[reviews.length-1]
   } 
-
+console.log(getLastReview(reviews))
 
 ///////////////🍔☕️🍽 STRETCH🍔☕️🍽////////////////////
 
